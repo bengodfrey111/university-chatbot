02b596d5fdb55be4d64520675df76f70d65d3129
David Lawal <kashlawal@Davids-MacBook-Pro.local>
2020-11-13 16:44:14 +0000

    changes to csv file

--- ToDoTextProcessing.py
+++ ToDoTextProcessing.py
@@ -1,7 +1,17 @@
 import todostorage, textProcessing
-File = todostorage.toDoRead()
-print(File)
+#File = todostorage.toDoRead()
+#print(File)
 def chatbotread(command):
     chatbotkeys = ["what", "who", "get", "grab", "show", ]
-    if textProcessing.inArray(chatbotkeys,command):""""BENS FUNCTION CHECKING IF CHATBOTKEYS ARRAY IS IN FUNCTION"""
+    keysNotStart = ["free", "not started"]
+    inProgress = ["in progress"]
+    CompletedTasks = ["Completed"]
+    ChatbotFile = ""
+    if (textProcessing.inArray(chatbotkeys, command)):#Bens FUNCTION
+        file = todostorage.toDoRead()
+        for i in range(0,len(file)):
+            ChatbotFile = ChatbotFile + "TaskId: " + file[i]["TaskID,"] + " Task: " + file[i]["Task"] + " DateStart: " + file[i]["DateStart"] + " DateDue: " + file[i]["DateDue"] + " User: " +  file[i]["User"] + " Completed: " + file[i]["Completed"]+ " TaskDifficulty: " + file[i]["TaskDifficulty"] +"\n"
+    return ChatbotFile
+
+print(chatbotread("what"))
 
--- todostorage.py
+++ todostorage.py
@@ -8,8 +8,8 @@ def toDoRead():
         lines.append(line)
     # lines[line][column]
     toDoList = []
-    for i in range(1,len(lines)):
-        toDo = {"TaskID" : lines[i][0], "Task" : lines[i][1], "DateStart" : lines[i][2], "DateDue" : lines[i][3], "User" : lines[i][4], "Completed": lines[i][5], "TaskDifficulty" : lines[i][6] }
+    for i in range(3,len(lines)):
+        toDo = {"TaskID" : lines[i][0], "Task" : lines[i][1], "DateStart" : lines[1][2], "DateDue" : lines[1][3], "User" : lines[i][2], "Completed": lines[i][3], "TaskDifficulty" : lines[i][4] }
         toDoList.append(toDo)
     return toDoList
 
